deb8f8a3391a1b1533e97c70b600f4d55c903a68
EryKlcarsln <eryklcarsln@gmail.com>
2021-12-27 11:48:56 +0300

    second change from Eray

--- src/test/java/com/cydeo/test/Eray2_test.java
+++ src/test/java/com/cydeo/test/Eray2_test.java
@@ -1,2 +1,8 @@
-package com.cydeo.test;public class Eray2_test {
+package com.cydeo.test;
+
+public class Eray2_test {
+
+    public static void main(String[] args) {
+        System.out.println("second change");
+    }
 }
