90cffe93a7ac7690cf6b262f2d5790b205315e0e
OnurHarmandar <harmandaronur@gmail.com>
2021-12-27 12:13:21 +0300

    Second day started

--- src/test/java/com/cydeo/test/Onur_Test.java
+++ src/test/java/com/cydeo/test/Onur_Test.java
@@ -3,5 +3,7 @@ package com.cydeo.test;
 public class Onur_Test {
     public static void main(String[] args) {
         System.out.println("Colobration Work Started");
+
+        System.out.println("Second day");
     }
 }
